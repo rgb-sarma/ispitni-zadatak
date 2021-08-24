0b128e1c0b7001ac32ce0e1e58beb674d65fd10a
weeb <petar.ilic1337@gmail.com>
2021-08-24 16:15:16 +0200

    about to do bullet test with me luck

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,101 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-navigation-drawer app v-model="drawer" temporary>
+      <!-- -->
+      <v-list nav dense>
+        <v-list-item-group v-model="group">
+          <v-list-item @click="toProfile">
+            <v-icon>
+              mdi-account-circle-outline
+            </v-icon>
+            <v-list-item-title>Profile</v-list-item-title>
+          </v-list-item>
+           <v-list-item @click="debug">
+            <v-icon>
+              mdi-clipboard-list-outline
+            </v-icon>
+            <v-list-item-title>Saved</v-list-item-title>
+          </v-list-item>
+           <v-list-item @click="debug">
+            <v-icon>
+              mdi-heart-outline
+            </v-icon>
+            <v-list-item-title>Orders</v-list-item-title>
+          </v-list-item>
+           <v-list-item @click="debug">
+            <v-icon>
+              mdi-power
+            </v-icon>
+            <v-list-item-title>Log Out</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+    <v-app-bar app>
+      <!-- -->
+      <v-btn fab plain @click="drawer = !drawer">
+        <md-icon>menu</md-icon>
+      </v-btn>
+      <v-btn @click="toHome">Home</v-btn>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn fab plain>
+        <md-icon>shopping_basket</md-icon>
       </v-btn>
     </v-app-bar>
 
+    <!-- Sizes your content based upon application components -->
     <v-main>
-      <router-view/>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
     </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+// import 'vue-material/dist/theme/default-dark.css'
+
+Vue.use(VueMaterial)
 
 export default Vue.extend({
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-});
+  data() {
+    return {
+      drawer: false,
+      group: null
+    }
+  },
+  watch: {
+    group(){
+      this.drawer = false
+    }
+  },
+  methods: {
+    debug() {
+      console.log("clicked");
+    },
+    toProfile(){
+      this.$router.push('/about')
+    },
+    toHome(){
+      this.$router.push('/')
+    }
+  },
+})
 </script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,42 @@
 <template>
-  <hello-world />
+  <div class="home-wrap">
+    <v-toolbar
+      color="primary"
+      light
+      extended
+      flat
+      src="../../public/pepe.jpg"
+      extension-height="150px"
+    >
+    </v-toolbar>
+    <v-card class="mx-auto"
+      max-width="700"
+      style="margin-top: -32px;">
+      <v-toolbar flat style="display: flex; align-items: center; justify-content: center;">
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-text-field v-model="search" clearable style="width: 500px;margin-top: 15px;">
+
+        </v-text-field>
+
+        <v-btn icon>
+          <v-icon>mdi-apps</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </v-card>
+
+  </div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
-  import HelloWorld from '../components/HelloWorld.vue'
+import Vue from 'vue'
 
   export default Vue.extend({
     name: 'Home',
 
     components: {
-      HelloWorld,
     },
   })
 </script>
